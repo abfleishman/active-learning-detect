66fa0aae54bca02113469ad0a57c3a47f1cf1c15
Abram B. Fleishman <abram@conservationmetrics.com>
2020-07-09 08:39:22 -0700

    add update label map

---
+++ train/active_learning_update_label_map.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Source environmental variables
+set -a
+sed -i 's/\r//g' $1
+. $1
+set +a
+# Make all necessary directories
+# mkdir -p $image_dir
+# Download all images
+# az storage blob download-batch --source $image_container_name --destination $image_dir
+# Create TFRecord from images + csv file on blob storage
+# TODO: Try to import create_predictions into this
+envsubst < $1 > cur_config.ini
+python ${python_file_directory}/update_label_map.py cur_config.ini 
